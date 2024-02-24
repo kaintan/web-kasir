2c6fb1129a652432a2e0f84bc9449fee99eaf23a
kaintan <rajwaafianto2308@gmail.com>
2024-02-24 12:37:07 +0700

    UKK 2024

---
+++ admin/modul/produk/paging.php
@@ -0,0 +1,117 @@
+<?php 
+$koneksi = mysqli_connect('localhost', 'root', '', 'kasir_zibran');
+global $koneksi;
+
+    $batas = 5;
+$halaman = isset($_GET['halaman']) ? (int)$_GET['halaman'] : 1;
+$halaman_awal = ($halaman>1) ? ($halaman * $batas) - $batas : 0;
+
+$previous = $halaman - 1;
+$next = $halaman + 1;
+
+$data = mysqli_query($koneksi, "SELECT * FROM tb_produk");
+$jumlah_data = mysqli_num_rows($data);
+$total_halaman = ceil($jumlah_data / $batas);
+
+
+$nomor = $halaman_awal+1;
+
+
+// cari
+if (isset($_POST['go'])) {
+  $cari = $_POST['cari'];
+  $data_siswa = mysqli_query($koneksi, "SELECT * FROM tb_produk WHERE nm_produk LIKE '%".$cari."%'");
+}else{
+  $data_siswa = mysqli_query($koneksi, "SELECT * FROM tb_produk LIMIT $halaman_awal, $batas");
+}
+
+
+foreach ($data_siswa as $pro):
+  ?>
+    
+
+
+
+<tr>
+                              <td><?= $i++;  ?></td>
+                              <td><?=  $pro['kdproduk'];?></td>
+                              <td><?=  $pro['nm_produk'];?></td>
+                              <td><?= $pro['kategori'];?></td>
+                              <td><?=  $pro['stok'];?></td>
+                              <td><?= $pro['rak'];?></td>
+                               <td><?= $pro['supplier'];?></td>
+
+                              <td><?php 
+
+                              if ($pro['stok'] > 0) {
+                                echo '<p style="color: green;">ada</p>';
+                              }else if($pro['stok']==0){
+                                echo '<p style="color: red;">habis</p>';
+                              }
+
+                              ?></td>
+                              <td><?= rupiah($pro['harga']); ?></td>
+
+                              
+                              <td>
+                                <!-- Trigger Modal Hapus -->
+                              <div data-toggle="modal" data-target="#hapus-produk<?= $pro['id'] ?>">
+                              <button type="button" class="btn btn-danger" data-toggle="tooltip" title="Hapus">
+                              <i class="fa fa-trash"></i>
+                              </button>
+                              </div>
+
+                              <!-- Modal Hapus -->
+                            <form action="" method="POST">
+                      <div class="modal fade" id="hapus-produk<?= $pro['id'] ?>" tabindex="-1" role="dialog"  aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                          <b><p class="modal-title" id="hapus-produk<?= $pro['id'] ?>" style="text-align: center; font-size: 18px;">Apakah anda yakin?</p></b>
+                        </div>
+                        <div class="modal-body">
+                          <div class="modal-body">
+                   
+                            <p>Kode Produk</p>
+                        <b><p><?= $pro['kdproduk'] ?></p></b>
+                        <p>Nama Produk</p>
+                        <b><p><?= $pro['nm_produk']; ?></p></b>
+                        <p>Stok</p>
+                        <b><p><?= $pro['stok']; ?></p></b>
+                          <p>Rak</p>
+                        <b><p><?= $pro['rak']; ?></p></b>
+                         <p>Supplier</p>
+                        <b><p><?= $pro['supplier']; ?></p></b>
+                         <p>Status</p>
+                        <b><p><?php 
+
+                        if ($pro['stok']==0) {
+                          echo '<p style="color: red;">habis</p>';
+                        }else{
+                            echo '<p style="color: green;">ada</p>';
+                        }
+
+                         ?></p></b>
+                         <p>Harga</p>
+                        <b><p><?= rupiah($pro['harga']); ?></p></b>
+                       
+                        
+                       
+                          <input type="hidden" name="id" value="<?= $pro['id'] ?>" class="form-control" hidden>
+                          </div>
+                         
+                        </div>
+                        <div class="modal-footer">
+                          <button type="submit" name="hapus" class="btn btn-danger">Hapus</button>
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                    </form><br>
+                    
+
+                              <?php endforeach; ?>
